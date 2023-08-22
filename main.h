ebc9691744fbdbfa1c544326e1e8364f5e955ecc
nyachoti-lastborn <jmogaka84@gmail.com>
2023-08-22 16:42:47 -0700

    Forgot main.h file

--- main.h
+++ main.h
@@ -1,1 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+/* _printf - Print formatted output to stdout
+ * @format: The format string
+ *
+ * Return: Number of characters printed (excluding the null byte)
+ */
 int _printf(const char *format, ...);
+
+/* main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void);
+
+#endif /* MAIN_H */
